1909f059c23c870faaafed43127b251daf611f6b
Chris Geroux <chris.m.geroux@gmail.com>
2018-04-20 17:54:46 +0000

    save state before terminating VMs

--- setup_requirements.sh
+++ setup_requirements.sh
@@ -1,7 +1,11 @@
 
 #!/bin/bash
 
-# Install requisite Ansible Galaxy roles
-ansible-galaxy install -r ./requirements.yml -p ./roles
+git clone https://github.com/geerlingguy/ansible-role-ntp.git roles/ntp;cd roles/ntp; git checkout 6a15884e9a6e05a9616b4c802251ffa3b1bc4d45;cd ../../
 
-exit 0
+
+
+# Install requisite Ansible Galaxy roles
+#ansible-galaxy install -r ./requirements.yml -p ./roles
+#
+#exit 0
